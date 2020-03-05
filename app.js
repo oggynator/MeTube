fd4c338c758e4316b9017d39d3f0d931c2afcdbc
oggynator <august.hejberg@gmail.com>
2020-03-05 13:25:30 +0100

    sdad

--- app.js
+++ app.js
@@ -7,10 +7,10 @@ app.get("/", (req,res)=>{
     return res.sendfile(__dirname + "/public/index.html");
 });
 
-app.listen(3000, error=>{
+app.listen(process.env.PORT, error=>{
     if(error){
         console.error(error);
     }
-    console.error("Server is alive and running on port:", 3000);
+    console.error("Server is alive and running on port:", process.env.PORT);
     
 });
