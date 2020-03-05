f1ab00389cb2c55c874b16324beeb13df43003a6
oggynator <august.hejberg@gmail.com>
2020-03-05 13:53:23 +0100

    yolo

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const app = express();
 
+app.use(express.static('public'));
+
+
 
 app.get("/", (req,res)=>{
     
-    return res.sendfile(__dirname + "/public/index.html");
+    return res.sendFile(__dirname + "/public/index.html");
+});
+
+app.get("/video", (req,res)=>{
+    
+    return res.sendFile(__dirname + "/public/video.html");
 });
 
 app.listen(process.env.PORT, error=>{
