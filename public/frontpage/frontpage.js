082c7bc8504bdd3d62bbb9ca0f51966401f065cb
oggynator <august.hejberg@gmail.com>
2020-04-16 08:35:30 +0200

    asda

--- public/frontpage/frontpage.js
+++ public/frontpage/frontpage.js
@@ -1,6 +1,4 @@
-
 $.get("videos?page=1", (response) => {
-    console.log(response.response);
     response.response.map((video) => {
         $("#video-gallery")
         .append(`<a href="/player/${video.fileName}">${video.title}</a>`);
